265674a96040d3f3899e1a56ce1e12cd60271b85
kartikshingde <kartikshingde12@gmail.com>
2025-01-01 13:00:56 +0530

    addes span

---
+++ 03/result.js
@@ -0,0 +1,40 @@
+let results = document.querySelector('#result');
+let input = document.querySelector('#inputScore');
+let button = document.querySelector('#Submit');
+
+const body=document.querySelector('body')
+
+button.addEventListener('click', function (e) {
+    let score = parseInt(input.value);  
+    
+    if (isNaN(score)) {
+        results.innerHTML = "Please enter a valid number for the score!!!";
+    } else {
+        Result(score);
+    }
+});
+
+function Result(score) {
+    if (score >= 80 && score <= 100) {
+        results.innerHTML = "First Class";
+        // body.style.backgroundColor='green'
+    } 
+    else if (score >= 60 && score < 80) {
+        results.innerHTML = "Second Class";
+        // body.style.backgroundColor='white'
+    } 
+    else if (score >= 40 && score < 60) {
+        results.innerHTML = "Pass, could be better...";
+        // body.style.backgroundColor='yellow'
+
+        
+    } 
+    else if (score > 100) {
+        results.innerHTML = "Invalid Marks";
+    } else {
+        results.innerHTML = "Fail, Jaa padhai kar...";
+        // body.style.backgroundColor='red'
+        
+        
+    }
+}
--- 03/script.js
+++ 03/script.js
@@ -1,40 +1,69 @@
-let results = document.querySelector('#result');
-let input = document.querySelector('#inputScore');
-let button = document.querySelector('#Submit');
+//*********Variables and Data Types*********//
 
-const body=document.querySelector('body')
+// number =>1,2,...etc
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
 
-button.addEventListener('click', function (e) {
-    let score = parseInt(input.value);  
+
+// object
+
+
+const student={
+    fullName:"kartik",
+    age:20,
+    cgpa:9.14,
+    isPass:true,
+
+};
+// console.log(student.cgpa)
+// console.log(student ["cgpa"])
+// student.fullName="Kartik Shingde"
+// console.log(student.fullName)
+const product={
+    title:"Ball Pen",
+    rating:4,
+    offer:5,
+    price:100,
+};
+profile={           //object=> key:value  pair 
+    fullName:"Kartik Shingde",
+    posts:190,
+    isFollow:true,
     
-    if (isNaN(score)) {
-        results.innerHTML = "Please enter a valid number for the score!!!";
-    } else {
-        Result(score);
-    }
-});
+    
+};
+let a=true
+console.log(typeof a);
+console.log(typeof isFollow);
 
-function Result(score) {
-    if (score >= 80 && score <= 100) {
-        results.innerHTML = "Your Result: First Class";
-        // body.style.backgroundColor='green'
-    } 
-    else if (score >= 60 && score < 80) {
-        results.innerHTML = "Your Result: Second Class";
-        // body.style.backgroundColor='white'
-    } 
-    else if (score >= 40 && score < 60) {
-        results.innerHTML = "Your Result: Pass, could be better...";
-        // body.style.backgroundColor='yellow'
 
-        
-    } 
-    else if (score > 100) {
-        results.innerHTML = "Invalid Marks";
-    } else {
-        results.innerHTML = "Your Result: Fail, Jaa padhai kar...";
-        // body.style.backgroundColor='red'
-        
-        
+let numArr=[4,9,7,3,4,6];
+
+// for(let i=0;i<numArr.length;i++){
+//     // alert("hello")
+//     console.log(numArr[i]);
+    
+// }
+
+
+
+// read Array Methods from MDN or w3school Docs...
+
+
+function getSquare(intArr){
+    let resultArr=[]
+    for(let j=0;j<intArr.length;j++){
+        resultArr.push(intArr[j]*intArr[j]);
+
     }
+    console.log(resultArr);
+
 }
+
+
+getSquare(numArr)
+
