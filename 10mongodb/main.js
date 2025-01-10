92dcfc5780b721b2b10bb967ec1e259022e5a44a
kartikshingde <kartikshingde12@gmail.com>
2025-01-10 15:21:29 +0530

    mongo-connection

--- 10mongodb/main.js
+++ 10mongodb/main.js
@@ -1,1 +1,46 @@
-    
+
+const express=require('express')
+const app=express()
+const PORT=4000
+const mongoose=require('mongoose')
+app.use(express.json())
+
+const MONGOURL="mongodb+srv://kartik:Kartik123@eygds.4kvnu.mongodb.net/EYGDS"
+
+mongoose.connect(MONGOURL,
+    ).then(()=>{
+    console.log("mongo db connected successfully")
+})
+
+const userSchema=new mongoose.Schema({
+    name:String,
+    age:Number,
+    email:String
+})
+
+const userModel=mongoose.model("users",userSchema)      //user Model
+
+app.post('/adduser',(req,res)=>{
+    const name=req.body.name
+    const age=req.body.age
+    const email=req.body.email
+    const user=userModel({
+        name:name,
+        age:age,
+        email:email
+    })
+    user.save().then(()=>{
+        res.send("user added")
+    })
+    res.send(`name is ${name}, age is ${age} and email is ${email}`)
+})
+
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Hello Mongo!</h1>")
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Port is running at port ${PORT}`)
+})
